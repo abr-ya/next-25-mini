0a324d2a3757e130744a6a3d39056a06e6550eac
abr-ya <gamer05@list.ru>
2025-10-04 15:46:24 +0300

    feat: Add Links to Main Page

--- app/_components/common/projects-links.tsx
+++ app/_components/common/projects-links.tsx
@@ -1,3 +1,11 @@
+import { ButtonLink } from "./button-link";
+
 export const ProjectsLinks = () => {
-  return <div>Projects Links ... (coming soon)</div>;
+  return (
+    <div>
+      <div>This Site's Projects Links:</div>
+      <ButtonLink variant="link" to="/cloud" text="Cloud Landing {first!}" />
+      <ButtonLink variant="link" to="/money" text="Money {in progress}" />
+    </div>
+  );
 };
