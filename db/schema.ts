943b23b1e4d0f095dbad9dbeb7d5ad254a150a59
abr-ya <gamer05@list.ru>
2025-10-08 21:57:02 +0300

    feat: Add tables to Drizzle shema

--- db/schema.ts
+++ db/schema.ts
@@ -23,3 +23,37 @@ export const transactionsTable = pgTable("transactions", {
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
 });
+
+// LinkList Part
+export const linkPagesTable = pgTable("link_pages", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: text("user_id").notNull(),
+  url: text().notNull(),
+  bgType: text("bg_type", {
+    enum: ["color", "image"],
+  }).notNull(),
+  bgColor: text("bg_color").notNull().default("#ffffff"),
+  bgImage: text("bg_image"),
+  displayName: text("display_name"),
+  location: text(),
+  bio: text(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
+
+export const linkItemsTable = pgTable("link_items", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  pageId: integer("page_id")
+    .references(() => linkPagesTable.id)
+    .notNull(),
+  title: text().notNull(),
+  url: text().notNull(),
+  description: text(),
+  order: integer().notNull().default(0),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
